09021750b9140c0ad258f052bfa0cb36c0ddae6f
MatiasJam <alegonza201800@gmail.com>
2020-12-17 08:35:01 -0300

    file ready

---
+++ pareimpares.js
@@ -0,0 +1,9 @@
+let pareImpares = function(num) {
+    if (num !== 0) {
+        num%2===0 ? console.log("El numero es par") : console.log("El numero es impar");
+    } else {
+        console.log("Error");
+    }
+}
+
+pareImpares(0)
